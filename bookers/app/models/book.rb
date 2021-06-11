a00f05e90b0aea68109b63be23aaba44404dfb20
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-11 02:51:18 +0000

    [update ☆機能]

--- bookers/app/models/book.rb
+++ bookers/app/models/book.rb
@@ -1,10 +1,10 @@
 class Book < ApplicationRecord
-  
-	has_many :reviews, dependent: :destroy
-	belongs_to :user, optional: true #Userモデルと1：N
+    
+ has_many :reviews, dependent: :destroy
+ belongs_to :user, optional: true #Userモデルと1：N
 
-	validates :title, presence: true
-	validates :body, presence: true, length: {maximum: 200}
+ validates :title, presence: true
+ validates :body, presence: true, length: {maximum: 200}
   
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
