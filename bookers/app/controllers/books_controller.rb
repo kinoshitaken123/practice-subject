614c4ca3ab6f39ccb4c0e9f65227574250f0b1d3
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-06-14 13:59:36 +0000

    [update]

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
+
   end
 
   def index
@@ -17,6 +18,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+
    
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
